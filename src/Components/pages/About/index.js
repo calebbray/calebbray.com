75b0b84c7b565d8ef5e173c1b992da9e36f67687
Caleb Bray <caleb.bray@economicmodeling.com>
2020-02-14 08:30:39 -0800

    remove unused hook import

--- src/Components/pages/About/index.js
+++ src/Components/pages/About/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import styled from 'styled-components';
 import WhiteSection from 'Components/atoms/WhiteSection';
 import SkillsCard from 'Components/molecules/SkillsCard';
